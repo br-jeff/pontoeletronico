48bd31d39bd2f764e8ac58c8706b15a46f063ec6
kalavh <45084197+kalavh@users.noreply.github.com>
2020-07-20 17:53:30 -0300

    logica para todas as marcações

--- Backend/Controladores/MarcaObra/funcoes/procuraMarca.js
+++ Backend/Controladores/MarcaObra/funcoes/procuraMarca.js
@@ -13,6 +13,76 @@ async function procuraMarca(numMarca,id){
         .then(event => marcacoes = event)
         .catch(err => res.json('Erro ao buscar marcação 1'))
     }
+    if(numMarca == 2) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca2: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 3) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca3: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 4) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca4: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 5) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca5: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 6) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca6: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 7) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca7: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
+    if(numMarca == 8) {
+        const jaMarcou = await marca.findAll({
+            attributes: ['id'],
+        where: {
+                id:id,
+                marca8: null,
+        }})
+        .then(event => marcacoes = event)
+        .catch(err => res.json('Erro ao buscar marcação 1'))
+    }
     return marcacoes
 
     };
