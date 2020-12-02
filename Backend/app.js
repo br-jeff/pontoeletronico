7397a08a2cab4808b8784583a4dbcad328fd0fed
kalavh <jefferson_1996Th@hotmail.com>
2020-12-02 11:04:27 -0300

    separete on app file

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const Sequelize = require('sequelize')
+require('dotenv').config();
+
+const rotaInicio = require('./Rotas/Inicio')
+const numMarcacao = require('./Rotas/numMarcacao')
+const marcaObra = require('./Rotas/rMarcaObra')
+
+app.use(express.json())
+
+app.get("/",rotaInicio)
+app.get("/marca/online/:idFunc/:numMarca", numMarcacao)
+
+app.get("/marcacao",marcaObra)
+app.post("/cria/marcacao",marcaObra)
+app.put("/marca/online/:idFunc",marcaObra)
+
+module.exports = app
--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,2 @@
-const express = require('express')
-const app = express()
-const Sequelize = require('sequelize')
-require('dotenv').config();
-//const sequelize = new Sequelize(require('./Bancodedados/bancocfg'))
-const bodyParser = require('body-parser')
-
-const rotaInicio = require('./Rotas/Inicio')
-const numMarcacao = require('./Rotas/numMarcacao')
-const marcaObra = require('./Rotas/rMarcaObra')
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-app.get("/",rotaInicio)
-app.get("/marca/online/:idFunc/:numMarca", numMarcacao)
-
-app.get("/marcacao",marcaObra)
-app.post("/cria/marcacao",marcaObra)
-app.put("/marca/online/:idFunc",marcaObra)
-
+const app = require('./app')
 app.listen(3000,()=>{console.log("servidor iniciado")})
