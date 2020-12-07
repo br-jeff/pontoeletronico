6497b8622cbadcef1d880de29458323db8992a6d
jeff <jefferson_1996Th@hotmail.com>
2020-12-07 16:34:33 -0300

    query check if user mark today

--- Backend/src/app/controller/markController.js
+++ Backend/src/app/controller/markController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const Mark = require('../models/Marks')
 module.exports = {
    async index(req,res){
         try{
@@ -11,16 +12,27 @@ module.exports = {
                     cpf
                 }
             })
-             
+            
             if(!checkUser) 
-                return res.json( {msg: 'erro usuario não cadastrado '  })
-                           
+                return res.json( {msg: 'erro usuario não cadastrado'  })
+            
+            let dateToday = new Date()
+
+            const checkUserMarkToday = await Mark.findOne({
+                where:{
+                   cpf,
+                   createdAt: dateToday
+                   }
+               })
+
+            if(!checkUserMarkToday)
+               return res.send('nao marcou hoje fazer logica')
             
 
             return res.json(checkUser) 
         }
         catch(err){
-            return res.json( {msg: 'erro ao tentar fazer marcação ' + err })
+            return res.json( {msg: 'erro ao tentar fazer marcação  ' + err })
         }
     }
 }
