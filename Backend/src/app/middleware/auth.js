c083837a7ba3bbc88bc7a3ee348f981a6254b63a
jeff <jefferson_1996Th@hotmail.com>
2020-12-14 21:59:05 -0300

    you just can create a user if you are adm with jwt

--- Backend/src/app/middleware/auth.js
+++ Backend/src/app/middleware/auth.js
@@ -1,12 +1,21 @@
+const { compare } = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
 module.exports = (req,res,next) => {
     const authToken = req.headers.authorization
     
     if(!authToken)
         return res.status(401).json({msg: 'Token é obrigatorio'})
 
-    token = authToken.split(' ')[1]
-        console.log(token)
-    return next()
+    let token = authToken.split(' ')[1]
+    let key = process.env.JWT_KEY
 
+    jwt.verify(token,key,(err,result)=>{
+        if(err)
+            return res.status(500).send('JWT Inválido')
+
+        return next()
+    })
+   
 }
