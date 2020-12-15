f70933e342424c1d7b15b4c495f34b1864569f3a
jeff <jefferson_1996Th@hotmail.com>
2020-12-14 21:03:25 -0300

    fix headers

--- Backend/src/app/middleware/auth.js
+++ Backend/src/app/middleware/auth.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken')
 module.exports = (req,res,next) => {
-    const authToken = req.header.authorization
+    const authToken = req.headers.authorization
     
     if(!authToken)
         return res.status(401).json({msg: 'Token é obrigatorio'})
-    
+
+    token = authToken.split(' ')[1]
+        console.log(token)
     return next()
 
 }
